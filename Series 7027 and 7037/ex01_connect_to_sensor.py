f8a9965cc120b18b18b62950c1eda5ae1bb72430
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2024-02-21 10:24:34 -0500

    Update ex01_connect_to_sensor.py
    Revised so that we're using only the driver code from the series_7027_7037.py file.
    
    Added boilerplate license information, a description of the example, removed the author.

--- Series 7027 and 7037/ex01_connect_to_sensor.py
+++ Series 7027 and 7037/ex01_connect_to_sensor.py
@@ -1,24 +1,60 @@
 """
-    Copyright 2024 Bird Technologies
-
+Example Description:
     This example shows how to use PyVisa to connect to and query informantion
     from a Series 7027 or 7037 Statistical Power Sensor.
 
+@verbatim
+
+The MIT License (MIT)
+
+Copyright (c) 2024 Bird
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@endverbatim
+
+@file series_7027_7037.py
+
+Copyright (c) Bird
+
 """
-import pyvisa as visa
+
+from series_7027_7037 import Series_7027
 import time
 
+mysensor = Series_7027()
+mysensor.connect("USB0::0x1422::0x7037::202100121::INSTR", 20000)
 
-MYSENSOR = "USB0::0x1422::0x7037::202100121::INSTR"
+# The query of the instrument ID happens within the connect()
+# method. Below, "private" member variables are used to acquire
+# the sensor info.
+print(mysensor._mfg_id)
+print(mysensor._model)
+print(mysensor._fw)
+print(mysensor._sn)
 
-rm = visa.ResourceManager()  # Open the resource manager.
+# But it is better to get this info using public member property
+# calls. 
+print(mysensor.manufacturer_id)
+print(mysensor.model_number)
+print(mysensor.firmware_version)
+print(mysensor.serial_number)
 
-series70x7 = rm.open_resource(MYSENSOR) # Open an instance of the sensor object.
-series70x7.timeout = 5000
-
-for j in range(1000):
-    print(series70x7.query("*IDN?\n").rstrip())
-    
-series70x7.close()
-
-rm.close()
+mysensor.disconnect()
+time.sleep(0.5)
