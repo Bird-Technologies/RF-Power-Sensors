2d672a1b67d2c3f0463b3c21d890d149b3a6ac21
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2024-05-17 10:33:00 -0400

    Update ex01_connect_and_communicate_using_pyserial.py
    1. Updated calibration status check to better avoid exception condition when not all chars get read.
    2. Updated set_measure_config() to correct for string write and better readback of status information.
    3. Revised to make get_sample_measurement_data().

--- Series 5000/ex01_connect_and_communicate_using_pyserial.py
+++ Series 5000/ex01_connect_and_communicate_using_pyserial.py
@@ -56,7 +56,8 @@ def get_instrument_identity(sp: serial):
 def get_calibration_status(sp: serial):
     status = True
     sp.write(b'F\r\n')
-    temp = sp.read_all().decode("utf-8").rstrip().split(',')
+    temp = sp.read_until(b'\r\n').decode("utf-8").rstrip().split(',')
+    #temp = sp.read_all().decode("utf-8").rstrip().split(',')
     if "NAK" in temp[1]:
         status = False
     return status
@@ -64,40 +65,62 @@ def get_calibration_status(sp: serial):
 def set_measurement_configuration(sp: serial,
                                   measType:MeasurementType=MeasurementType.measAverage,
                                   dboffset:float=0.0,
-                                  filtervalue:float=Filter.medium,
-                                  measunits:PowerUnits=PowerUnits.unit_w,
+                                  filtervalue:float=Filter.high,
+                                  measunits:PowerUnits=PowerUnits.unit_auto_w,
                                   ccdflimit:float=0.0e+3):
+    status = True
     # Build the configuration command string...
-    cmd = f"G,0{measType.value},{dboffset:0.5e},{filtervalue.value:0.5e},{measunits.value},{ccdflimit:0.5e},\r\n"
+    cmd = f"G,0{measType.value},{dboffset:0.5e},{filtervalue.value:0.5e},{measunits.value},{ccdflimit:0.5e}\r\n"
     sp.write(cmd.encode())
     
-    temp = sp.read_all().decode("utf-8").rstrip()
-    if "NAK" in temp:
+    temp = sp.read_until(b'\r\n').decode("utf-8").rstrip().split(',')
+    #temp = sp.read_all().decode("utf-8").rstrip()
+    if "NAK" in temp[2]:
         status = False
     return status
 
-def get_measurement_data(sp: serial):
-    sp.flush()
-    sleep(0.1)
-    sp.write(b'T\n')
-    temp = sp.read_all()
+def get_sample_measurement_data(sp: serial):
+    status = True
+    sp.write(b'T\r\n')
+    temp = sp.read_until(b'\r\n').decode("utf-8").rstrip().split(',')
+
+    cmd = temp[0]
+    burst_pwr = float(temp[1])
+    temperature = float(temp[2])
+    fwd_pwr = float(temp[3])
+    rfl_pwr = float(temp[4])
+    peak_pwr = float(temp[5])
+    filer_value = float(temp[6])
+    meas_type = int(temp[7])
+    units = temp[8].strip()
+    ccdf_factor = float(temp[9])
+    crest_factor = float(temp[10])
+    duty_cycle = float(temp[11])
+    dud = temp[12]
+    state = temp[13]
+    if "NAK" in temp[13]:
+        status = False
     
-    sleep(0.5)
-    templist = temp.decode("utf-8").rstrip().split(',')
-    for item in templist:
-        print(f'{item}')
-    print(temp)
+    return status, burst_pwr, temperature, fwd_pwr, rfl_pwr, peak_pwr, filer_value, meas_type, units, ccdf_factor, crest_factor, duty_cycle
 
-my5000 = serial.Serial(port='COM3', baudrate=9600, parity="N", stopbits=1,bytesize=8)
-#my5000.flush()
+def get_streamed_measurement_data(sp:serial):
+    return True
 
-set_measurement_configuration(my5000, MeasurementType.measAverage, 1.23458789e0, filtervalue=Filter.medium, measunits=PowerUnits.unit_auto_w, ccdflimit=0.0e+3)
+my5000 = serial.Serial(port='COM8', baudrate=9600, parity="N", stopbits=1,bytesize=8)
 
 smodel, sdate, sversion, scomms = get_instrument_identity(my5000)
 print(f"Model: {smodel}\nFW Version: {sversion}\nFW Date: {sdate}\nComms Type: {scomms}")
 
 print(get_calibration_status(my5000))
 
-get_measurement_data(my5000)
+set_measurement_configuration(my5000,
+                              MeasurementType.measAverage,
+                              0.0,
+                              filtervalue=Filter.medium,
+                              measunits=PowerUnits.unit_auto_w,
+                              ccdflimit=150.0)
 
+status, burst_pwr, temperature, fwd_pwr, rfl_pwr, peak_pwr, filer_value, meas_type, units, ccdf_factor, crest_factor, duty_cycle = get_sample_measurement_data(my5000)
+
+print(f"Sampled measurement:\n\tBurst Power: {burst_pwr}\n\tTemperature: {temperature}\n\tForward Power: {fwd_pwr}\n\tReflected Power: {rfl_pwr}")
 my5000.close()
